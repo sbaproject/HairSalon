2916463f07c61332089b03c7e6659e3b20314509
ducsba <ndduc@starboardasia.com>
2020-03-17 10:00:20 +0700

    update code master pages

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+@section('title','Login')
+@section('menu')
+@parent
+@endsection
+@section('content')
+<div class="templatemo-content-container">
+          
+                  
+                  <tr>
+                    <td><?php echo "Đang phát triển"; ?></td>
+                    <td><a href="" class="templatemo-edit-btn">Edit</a>
+                   <a href="" class="templatemo-edit-btn">Delete</a></td>
+                  </tr>   
+          
+@endsection
