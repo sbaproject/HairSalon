29afefb23a7b1c0874a8223b1bfd991f2c4003c7
dtnthach <dtnthach@starboardasia.com>
2020-03-17 10:05:40 +0700

    add model

--- app/Shop.php
+++ app/Shop.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     protected $table = "t_shop";
+    
+    public function staff(){
+        return $this->hasMany('App\Staff','s_shop','sh_id');
+    }
 }
--- app/Staff.php
+++ app/Staff.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Staff extends Model
 {
      protected $table = "t_staff";
+     
+     public function Shop(){
+         return $this->belongsTo('App\Shop','s_shop','s_id');
+     }
 }
