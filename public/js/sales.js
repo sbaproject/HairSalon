647a8fe097c8eeed202a2090624702b9dd3f50a8
dtnthach <dtnthach@starboardasia.com>
2020-03-23 23:34:09 +0700

    update sales.js

--- public/js/sales.js
+++ public/js/sales.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
     $('input[name="s_money"]').number( true, 0 );
 
-    $(".statusResult").fadeOut( 5000, function() {
+    $(".statusResult").fadeOut( 7000, function() {
         $("#statusResult").attr('class', 'statusBefore');
         $("#statusResult").html('');
         $("#statusResult").show()
